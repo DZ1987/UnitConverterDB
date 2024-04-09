248562f8d6e23a639f95e16a32f35c788847ae00
DZ <zimmerli1987@students.cptc.edu>
2024-04-09 10:01:27 -0700

    Added some Seed Data to the Database.

--- QuarterProject/Data/ApplicationDbContext.cs
+++ QuarterProject/Data/ApplicationDbContext.cs
@@ -12,5 +12,17 @@ namespace QuarterProject.Data
         }
 
         public DbSet<RegisteredUser> RegisteredUser { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Calls the OnModelCreating method of the base DbContext class.
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RegisteredUser>().HasData(
+                new RegisteredUser { Id = 1, UserName = "Admin", UserText = "T E S T\r\nS     E\r\nE     S\r\nT S E T" },
+                new RegisteredUser { Id = 2, UserName = "Admin", UserText = "TTTTTTTTTT EEEEEEEE SSSSSSSS TTTTTTTTTT\r\n    TT     EE       SS           TT\r\n    TT     EEEEEEE  SSSSSSSS     TT\r\n    TT     EE             SS     TT\r\n    TT     EEEEEEEE SSSSSSSS     TT" },
+                new RegisteredUser { Id = 3, UserName = "Admin", UserText = "<tbody>\r\n    @foreach (var item in Model)\r\n    {\r\n        <tr>\r\n            <td class=\"col-1 border-right\">\r\n                <a asp-action=\"Edit\" asp-route-id=\"@item.Id\">Edit</a><br>\r\n                <a asp-action=\"Details\" asp-route-id=\"@item.Id\">Details</a><br>\r\n                <a asp-action=\"Delete\" asp-route-id=\"@item.Id\">Delete</a>\r\n            </td>\r\n            <td>\r\n                <pre>@Html.DisplayFor(modelItem => item.UserText)</pre>\r\n            </td>\r\n        </tr>\r\n    }\r\n</tbody>" }
+            );
+        }
     }
 }
---
+++ QuarterProject/Data/Migrations/20240409165802_InitialSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace QuarterProject.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "RegisteredUser",
+                columns: new[] { "Id", "UserName", "UserText" },
+                values: new object[,]
+                {
+                    { 1, "Admin", "T E S T\r\nS     E\r\nE     S\r\nT S E T" },
+                    { 2, "Admin", "TTTTTTTTTT EEEEEEEE SSSSSSSS TTTTTTTTTT\r\n    TT     EE       SS           TT\r\n    TT     EEEEEEE  SSSSSSSS     TT\r\n    TT     EE             SS     TT\r\n    TT     EEEEEEEE SSSSSSSS     TT" },
+                    { 3, "Admin", "<tbody>\r\n    @foreach (var item in Model)\r\n    {\r\n        <tr>\r\n            <td class=\"col-1 border-right\">\r\n                <a asp-action=\"Edit\" asp-route-id=\"@item.Id\">Edit</a><br>\r\n                <a asp-action=\"Details\" asp-route-id=\"@item.Id\">Details</a><br>\r\n                <a asp-action=\"Delete\" asp-route-id=\"@item.Id\">Delete</a>\r\n            </td>\r\n            <td>\r\n                <pre>@Html.DisplayFor(modelItem => item.UserText)</pre>\r\n            </td>\r\n        </tr>\r\n    }\r\n</tbody>" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RegisteredUser",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "RegisteredUser",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "RegisteredUser",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
