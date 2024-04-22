556667ab6e90b0e2860ef865367201f3ba70f4d6
DZ <zimmerli1987@students.cptc.edu>
2024-04-22 10:26:34 -0700

    Added ConvertTemperature Model Class.

---
+++ QuarterProject/Models/ConvertTemperature.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuarterProject.Models
+{
+    /// <summary>
+    /// Represents a temperature conversion model.
+    /// </summary>
+    public class ConvertTemperature
+    {
+        /// <summary>
+        /// The unique identifier for a temperature conversion.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Represents the name of the temperature scale.
+        /// </summary>
+        public required string TemperatureName { get; set; }
+
+        /// <summary>
+        /// Represents the conversion method name from one temperature scale to another.
+        /// </summary>
+        public required string TemperatureConversion { get; set; }
+
+        /// <summary>
+        /// Represents the mathematical formula used for the temperature conversion.
+        /// </summary>
+        public required string TemperatureFormula { get; set; }
+    }
+}
