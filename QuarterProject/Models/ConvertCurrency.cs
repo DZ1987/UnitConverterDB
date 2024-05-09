99e9c8adc9ebbb8941e5992ae2d5df848a050237
DZ <zimmerli1987@students.cptc.edu>
2024-05-09 14:06:18 -0700

    Added ConvertCurrency Model Class.

---
+++ QuarterProject/Models/ConvertCurrency.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuarterProject.Models
+{
+    /// <summary>
+    /// Represents a currency conversion model.
+    /// </summary>
+    public class ConvertCurrency
+    {
+        /// <summary>
+        /// The currency code (ISO 4217).
+        /// </summary>
+        [Key]
+        [StringLength(3)]
+        public required string CurrencyCode { get; set; }
+
+        /// <summary>
+        /// The currency name.
+        /// </summary>
+        public required string CurrencyName { get; set; }
+    }
+}
