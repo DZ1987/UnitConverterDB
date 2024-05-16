8eb9a529488aee249de48542cd8306c0e73c8a49
DZ <zimmerli1987@students.cptc.edu>
2024-05-16 15:34:40 -0700

    Added "UserTextTitle" to RegisteredUser Model Class.

--- QuarterProject/Models/RegisteredUser.cs
+++ QuarterProject/Models/RegisteredUser.cs
@@ -20,6 +20,12 @@ namespace QuarterProject.Models
         public required string UserName { get; set; }
 
         /// <summary>
+        /// The User entered text title.
+        /// </summary>
+        [StringLength(40)]
+        public required string UserTextTitle { get; set; } = "No Title";
+
+        /// <summary>
         /// The User entered text.
         /// </summary>
         [StringLength(1000)]
