e74c3d29cb39e30d566cbaaad8d7bc138bd82334
DZ <zimmerli1987@students.cptc.edu>
2024-07-11 13:27:16 -0700

    Added Statistics Model Class.

---
+++ UnitConverterDB/Models/Statistics.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnitConverterDB.Models
+{
+    /// <summary>
+    /// Represents a statistics model. 
+    /// </summary>
+    public class Statistics
+    {
+        /// <summary>
+        /// The unsorted numbers.
+        /// </summary>
+        [Key]
+        public string Numbers { get; set; }
+
+        /// <summary>
+        /// The sorted numbers.
+        /// </summary>
+        public List<int> NumbersSorted { get; set; }
+    }
+}
